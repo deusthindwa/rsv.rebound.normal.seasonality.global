b7ccfae6c98dcdf3f94f135b5955330b6c0b1401
deusthindwa <deus.thindwa@gmail.com>
2022-11-29 00:31:54 +0200

    mlw

--- script/6_manipRSVdata.R
+++ script/6_manipRSVdata.R
@@ -12,7 +12,7 @@ rsvds %>%
   ggplot(aes(x = date(date), y = cases)) +
   geom_line() + 
   facet_wrap(. ~ country, scales = "free_y") +
-  theme_bw(base_size = 14, base_family = "Lato") +
+  theme_bw(base_size = 10, base_family = "Lato") +
   labs(title = , x = "MMWR Date", y = "RSV cases")
 
 #=========================================================
@@ -31,7 +31,7 @@ rsvds %>%
   ggplot(aes(x = date(date), y = cases)) +
   geom_line() + 
   facet_wrap(. ~ country, scales = "free_y") +
-  theme_bw(base_size = 14, base_family = "Lato") +
+  theme_bw(base_size = 10, base_family = "Lato") +
   labs(title = , x = "MMWR Date", y = "RSV cases")
 
 #=========================================================
@@ -47,9 +47,55 @@ rsvds %>%
   ggplot(aes(x = date(date), y = cases)) +
   geom_line() + 
   facet_wrap(. ~ country, scales = "free_y") +
-  theme_bw(base_size = 14, base_family = "Lato") +
+  theme_bw(base_size = 10, base_family = "Lato") +
   labs(title = , x = "MMWR Date", y = "RSV cases")
 
 #=========================================================
 
+#plotting RSV cases in Africa/South East Asia/Middle East/Western pacific
+rsvds %>%
+  dplyr::filter(country %in% c("Cameroon", "Central African Republic", "Côte d'Ivoire", "Madagascar",
+                               "Mozambique", "Uganda", "South Africa", 
+                               "India",
+                               "Australia", "Japan", "Mongolia", "Malaysia",
+                               "Oman", "Qatar")) %>%
+  ggplot(aes(x = factor(month(date)), y = cases)) +
+  geom_line() + 
+  facet_wrap(. ~ country, scales = "free_y") +
+  theme_bw(base_size = 10, base_family = "Lato") +
+  labs(title = , x = "MMWR Date", y = "RSV cases")
 
+#=========================================================
+
+#plotting RSV cases in Europe
+rsvds %>%
+  dplyr::filter(
+    country %in% c("France", "Germany", "Netherlands", "Spain", "Portugal", "Iceland",
+                   "Ireland", "Denmark", "Finland", "Sweden", "Malta", "United Kingdom, England", 
+                   "United Kingdom, Northern Ireland", "United Kingdom, Scotland",
+                   "Bulgaria", "Belarus", "Russian Federation", "Hungary", "Poland", "Slovakia")) %>%
+  mutate(country = if_else(country == "United Kingdom, Northern Ireland", "N Ireland",
+                           if_else(country == "United Kingdom, Scotland", "Scotland",
+                                   if_else(country == "United Kingdom, England", "England",
+                                           if_else(country == "Russian Federation","Russia", country))))) %>%
+  ggplot(aes(x = factor(month(date)), y = cases)) +
+  geom_line() + 
+  facet_wrap(. ~ country, scales = "free_y") +
+  theme_bw(base_size = 10, base_family = "Lato") +
+  labs(title = , x = "MMWR Date", y = "RSV cases")
+
+#=========================================================
+
+#plotting RSV cases in Americas
+rsvds %>%
+  dplyr::filter(
+    country %in% c("Argentina", "Belize", "Bolivia (Plurinational State of)", "Brazil", "Canada", "Colombia", "Costa Rica",
+                   "Dominican Republic", "Ecuador", "El Salvador", "Guatemala", "Honduras", "Mexico",
+                   "Nicaragua", "Panama", "Paraguay", "Peru", "Uruguay")) %>%
+  mutate(country = if_else(country == "Bolivia (Plurinational State of)", "Bolivia",
+                           if_else(country == "Dominican Republic", "Dominica", country))) %>%
+  ggplot(aes(x = factor(month(date)), y = cases)) +
+  geom_line() + 
+  facet_wrap(. ~ country, scales = "free_y") +
+  theme_bw(base_size = 10, base_family = "Lato") +
+  labs(title = , x = "MMWR Date", y = "RSV cases")
